4517189df4b4f57b93c4ad932fec921f54c67278
Katawut Utaipaisalwong <katawut@apples-iMac.local>
2018-02-16 19:02:22 +0700

    update code

--- server/routes/redirect.js
+++ server/routes/redirect.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
+const unescape = require('validator/lib/unescape');
 
 const config = require('../config/config');
 const base58 = require('../utils/base58');
 const Url = require('../models/url');
 
+
 /* Get short URL and redirect to original link */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
 
     var shortUrl = req.params.short_url;
+
+    // decode short url strings to unique ID for find original URL in database
     var id = base58.decode(shortUrl);
 
     // check if url already exists in database
     Url.findOne({_id: id}, function (err, doc){
       if (doc) {
-        // found an entry in the DB, redirect the user to their destination
-        res.redirect(doc.long_url);
+        // found an entry in the DB, encoded html entities and redirect the user to their destination
+        return res.redirect(unescape(doc.long_url));
       } else {
-        // nothing found, take 'em home
-        res.redirect(config.webhost);
+        // if not found, redirect to homepage
+        return res.redirect(config.webhost);
       }
     });
 
