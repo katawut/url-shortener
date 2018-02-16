1fcff502610115973bedbe72ae824624efc20bd4
Katawut Utaipaisalwong <katawut@apples-iMac.local>
2018-02-16 18:04:13 +0700

    setup security

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+// module for security
+const helmet = require('helmet');
+const Ddos = require('ddos');
+
+// application module
 const redirect = require('./server/routes/redirect');
 const shorten = require('./server/routes/shorten');
-const base58 = require('./server/utils/base58');
 const config = require('./server/config/config');
 
+var ddos = new Ddos;
 const app = express();
 
 mongoose.connect(`mongodb://${config.db.host}/${config.db.name}`);
@@ -16,10 +22,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'server/public')));
 
+// --- Security setup ---
+
+//Set up Content-Security-Policy rule for style and script sources
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", 'maxcdn.bootstrapcdn.com'],
+      scriptSrc: ["'self'", 'code.jquery.com', 'maxcdn.bootstrapcdn.com' ]
+    }
+  }))
+// XSS Filter
+app.use(helmet.xssFilter()) 
+// Prevent iframe clickjacking
+app.use(helmet.frameguard({ action: 'sameorigin' }));
+// Disable Powered-By
+app.use(helmet.hidePoweredBy())
+// Prevent browser for trying guess the MIME Type
+app.use(helmet.noSniff())
+// Basic prevent DDOS attack or unfinished loop requests from user
+app.use(ddos.express);
 
 // --- Application route ---
 
-// homepage
+// home and Form page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'server/views/index.html'));
 })
