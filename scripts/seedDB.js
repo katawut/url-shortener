3e8c04722d32989998995a43839640897f3ba383
Katawut Utaipaisalwong <katawut@apples-iMac.local>
2018-02-16 11:45:28 +0700

    update code

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,26 +1,39 @@
+/**
+ * SeedDB
+ * This script is for seed first entry to counters collection for use as a unique key 
+ * for original URL in urls collection
+ */
+
 const MongoClient = require('mongodb').MongoClient;
 const config = require('../server/config/config');
+
+// init your first sequence number
+const seedNum = 10000;
 
 var url = `mongodb://${config.db.host}/${config.db.name}`;
 
 MongoClient.connect(url, (err, db) => {
     if (err) {
-        console.log(err);
+        console.log(err.message);
+        return;
     }
     var dbo = db.db(config.db.name);
     dbo.createCollection('counters', (err, res) => {
         if (err) {
-            console.log(err);
+            console.log(err.message);
             db.close();
+            return;
         }
-        var seedData = { _id: "url_count", seq: 10000 };
+        var seedData = { _id: "url_count", seq: seedNum };
         dbo.collection('counters').insertOne(seedData, (err, res) => {
             if (err) {
-                console.log(err);
-                db.close(); 
+                console.log(err.message);
+                db.close();
+                return;
             }
             console.log('Seed data completed');
             db.close();
         })
     })
+
 });
