c9e39bfa86a6d19c3d0adc9b55fa37cff25f93d2
Katawut Utaipaisalwong <katawut@apples-iMac.local>
2018-02-16 18:58:45 +0700

    update code

--- server/public/javascripts/shorten.js
+++ server/public/javascripts/shorten.js
@@ -1,16 +1,29 @@
 // send original url for generate the url shortener
-$('.btn').on('click', function() {
-    $('#result').text('Processing...');
-    $.ajax({
-        url: '/api/shorten',
-        type: 'POST',
-        dataType: 'JSON',
-        data: {url: $('#url-field').val()},
-        success: function(data) {
-            var resultHTML = '<strong>Your shorten url is:</strong> <a class="result" href="' + data.shortUrl + '">'
-            + data.shortUrl + '</a>';
-            $('#result').html(resultHTML);
-            $('#result').hide().fadeIn('slow');
-        }
-    }) 
+$('.btn').on('click', function(e) {
+
+    if ( $('#url-field').val() === '' ) {
+        e.preventDefault();
+    } else {
+        $('#result').text('Processing...');
+        $.ajax({
+            url: '/api/shorten',
+            type: 'POST',
+            dataType: 'JSON',
+            data: {url: $('#url-field').val()},
+            success: function(data) {
+                var resultHTML = '';
+
+                if (data.error) {
+                    resultHTML = '<p class="errorMsg">'+data.result+'</p>';
+                } else {
+                    resultHTML = '<strong>Your shorten url is:</strong> <a class="result" href="' + data.result + '">'
+                + data.result + '</a>';
+                }
+                
+                $('#result').html(resultHTML);
+                $('#result').hide().fadeIn('slow');
+            }
+        }) 
+    }
+
 });
